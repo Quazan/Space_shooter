4dd71b449ead9fb6212deea3ccc87a1cc9c340e6
Grzegorz Aleksiuk <01133295@pw.edu.pl>
2019-05-22 22:21:10 +0200

    Update Missile.java

--- src/proz/game/model/Missile.java
+++ src/proz/game/model/Missile.java
@@ -2,19 +2,19 @@ package proz.game.model;
 
 import java.awt.*;
 
-public class Missile extends GameObject{
+public class Missile extends GameObject {
 
-    public Missile(int startX, int startY){
+    public Missile(int startX, int startY) {
         super(startX, startY);
     }
 
     @Override
-    public Image getImage(){
+    public Image getImage() {
         return imageIcon.getImage();
     }
 
     @Override
-    public void setImage(){
+    public void setImage() {
         String str = "assets\\PNG\\Lasers\\laserRed03.png";
         loadImage(str);
         getDimensions();
